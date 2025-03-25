24d146114faa055767f9cb96065e7122477d5ee3
Rafa Khadafi <=>
2025-03-25 10:42:09 +0700

    nambah oop

--- JAVA_SCRIPT_2025/LATIHAN/7.ubah array_jadi objek.js
+++ JAVA_SCRIPT_2025/LATIHAN/7.ubah array_jadi objek.js
@@ -10,3 +10,12 @@ let tes2 = tes.reduce((acc, [key, value]) => {
 },{});
 
 console.log(tes2);
+
+
+// ini pake map
+const apiData = [["id", 1], ["name", "Alice"], ["role", "Developer"]];
+const hasil = apiData.map(([key, value]) => {
+  return {[key] : [value]}
+})
+
+console.log(hasil)
